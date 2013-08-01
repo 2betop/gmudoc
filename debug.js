47459aedb5ec1bba4ad2d256eee5bddb87611938
liaoxuezhi <liaoxuezhi@baidu.com>
2013-08-01 14:15:11 +0800

    add draft

--- debug.js
+++ debug.js
@@ -4,12 +4,10 @@
     var Doc = require('./lib/doc.js');
 
     var ins = new Doc({
-        cwd: '../gmu/src',
-        files: ['core/*.js']
+        cwd: './test/samples',
+        files: ['sample4.js', 'sample5.js']
     });
 
     ins.run();
-    
-    debugger;
 
 })();
--- test/samples/sample2.js
+++ test/samples/sample2.js
@@ -11,15 +11,30 @@
  * @class Klass1
  * @module Module1
  * @description 这是类说明
+ * @static
+ */
+
+/**
+ * @class Klass2
+ * @module Module1
+ * @description 这是类说明
+ * @constructor
  */
 
 /**
  * @method funA
+ * @param {String} [var1=3] var1说明
+ * @param {Object} [var2] var2说明
+ * @param {Boolean} [var2.subvar1] sub var 1说明
+ * @param {Boolean} [var2.subvar2] sub var 2说明
+ * @return {Boolean} 当xxx的时候返回boolean值
+ * @return {self} 当xxx的时候返回本身
  * @description 这是方法说明
  */
 
 /**
  * @event ready
+ * @param {Event} e event对象
  * @description 这是事件说明
  */
 
@@ -30,4 +45,5 @@
 
 /**
  * @property {String} abc 描述
+ * @type
  */
